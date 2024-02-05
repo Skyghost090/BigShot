d433e1e9addd1841a0bd88eb4e041a6be6b24c31
Klebinhop <120269841+Klebinhop@users.noreply.github.com>
2024-02-05 19:45:57 -0300

    Add files via upload

--- bigshot.sh
+++ bigshot.sh
@@ -35,7 +35,6 @@ function disable {
 }
 
 case $1 in
-    "performace") performace ;;
     "eficient") eficient ;;
     "disable") disable ;;
     "*") echo "Unknown Command" ;;
--- service.sh
+++ service.sh
@@ -1,7 +1,6 @@
 count=0
 
 while [ -n $count ]; do
-    timeout 2s getevent | grep '0001 0073 00000001' && timeout 2s getevent | grep '0001 0073 00000001' && sh 'bigshot.sh' performace
+    timeout 2s getevent | grep '0001 0073 00000001' && timeout 2s getevent | grep '0001 0073 00000001' && sh 'bigshot.sh' disable
     timeout 2s getevent | grep '0001 0072' && timeout 2s getevent | grep '0001 0072' && sh 'bigshot.sh' eficient
-    timeout 2s getevent | grep '0001 0074 00000001' && timeout 2s getevent | grep '0001 0074 00000001' && sh 'bigshot.sh' disable
 done
