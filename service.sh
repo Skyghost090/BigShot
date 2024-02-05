b1d8db12706553b4f9b36428e2e603c27221958c
Klebinhop <120269841+Klebinhop@users.noreply.github.com>
2024-02-05 19:38:25 -0300

    Add files via upload

--- service.sh
+++ service.sh
@@ -1,42 +1,7 @@
-function kill_background_apps {
-    CurrentApp=$(dumpsys window | grep mCurrentFocus | tr -d '}' | tr -d '{' | tr -d '=' | cut -c33- | rev | cut -d '/' -f2-2 | rev)
-    UserPackagesList=$(pm list packages -3 | cut -c9- | tr "\n" " ")
-    ParableApps=$(echo $UserPackagesList | sed "s/$(echo $CurrentApp | cut -d ' ' -f2)/ /g")
-    for str in ${ParableApps[@]}; do
-        pm disable-user --user 0 $str
-    done
-    pm disable-user --user 0 com.google.android.gms
-}
+count=0
 
-function open_background_apps {
-    CurrentApp=$(dumpsys window | grep mCurrentFocus | tr -d '}' | tr -d '{' | tr -d '=' | cut -c33- | rev | cut -d '/' -f2-2 | rev)
-    UserPackagesList=$(pm list packages -3 | cut -c9- | tr "\n" " ")
-    for str in ${UserPackagesList[@]}; do
-        pm enable $str
-    done
-    pm enable com.google.android.gms
-}
-
-function performace {
-    kill_background_apps
-}
-
-function eficient {
-    kill_background_apps
-    wm size $(($(wm size | awk '{print $3}' | cut -dx -f1) / 3 * 2))x$(($(wm size | awk '{print $3}' | cut -dx -f2) / 3 * 2))
-    wm density $(($(wm density | sed -n '1p' | cut -c19-) / 3 * 2))
-
-}
-
-function disable {
-    open_background_apps
-    wm size reset
-    wm density reset
-}
-
-case $1 in
-    "performace") performace ;;
-    "eficient") eficient ;;
-    "disable") disable ;;
-    "*") echo "Unknown Command" ;;
-esac
+while [ -n $count ]; do
+    timeout 2s getevent | grep '0001 0073 00000001' && timeout 2s getevent | grep '0001 0073 00000001' && sh 'bigshot.sh' performace
+    timeout 2s getevent | grep '0001 0072' && timeout 2s getevent | grep '0001 0072' && sh 'bigshot.sh' eficient
+    timeout 2s getevent | grep '0001 0074 00000001' && timeout 2s getevent | grep '0001 0074 00000001' && sh 'bigshot.sh' disable
+done
