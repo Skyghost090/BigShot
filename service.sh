37f8fe32d6ddb9744e8b474903589d3cfc3a9d41
Klebinhop <120269841+Klebinhop@users.noreply.github.com>
2024-02-06 19:00:42 -0300

    Delete service.sh

--- service.sh
+++
@@ -1,6 +0,0 @@
-count=0
-
-while [ -n $count ]; do
-    timeout 2s getevent | grep '0001 0073 00000001' && timeout 2s getevent | grep '0001 0073 00000001' && sh 'bigshot.sh' disable
-    timeout 2s getevent | grep '0001 0072' && timeout 2s getevent | grep '0001 0072' && sh 'bigshot.sh' eficient
-done
