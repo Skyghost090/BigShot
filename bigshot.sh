86e574b6b0272a68ea72a6d26131a749c00427bd
Klebinhop <120269841+Klebinhop@users.noreply.github.com>
2024-04-24 11:27:27 -0300

    Add more 4 apps params

--- bigshot.sh
+++ bigshot.sh
@@ -1,19 +1,41 @@
+param2=$2
+param3=$3
+param4=$4
+param5=$5
+param6=$6
+param7=$7
 function kill_background_apps {
-    CurrentApp=$(dumpsys window | grep mCurrentFocus | tr -d '}' | tr -d '{' | tr -d '=' | cut -c33- | rev | cut -d '/' -f2-2 | rev)
+    echo $param2
+
+    if [[ $param2 = "" ]]; then
+        echo "Please type id from the app"
+        exit 3
+    fi
+
     UserPackagesList=$(pm list packages -3 | cut -c9- | tr "\n" " ")
-    ParableApps=$(echo $UserPackagesList | sed "s/$(echo $CurrentApp | cut -d ' ' -f2)/ /g")
-    for str in ${ParableApps[@]}; do
+
+    for str in ${UserPackagesList[@]}; do
         pm disable-user --user 0 $str
     done
+
     pm disable-user --user 0 com.google.android.gms
+    pm enable $param2
+    pm enable $param3
+    pm enable $param2
+    pm enable $param3
+    pm enable $param4
+    pm enable $param5
+    pm enable $param6
+    pm enable $param7
 }
 
 function open_background_apps {
-    CurrentApp=$(dumpsys window | grep mCurrentFocus | tr -d '}' | tr -d '{' | tr -d '=' | cut -c33- | rev | cut -d '/' -f2-2 | rev)
     UserPackagesList=$(pm list packages -3 | cut -c9- | tr "\n" " ")
+
     for str in ${UserPackagesList[@]}; do
         pm enable $str
     done
+
     pm enable com.google.android.gms
 }
 
@@ -25,7 +47,6 @@ function eficient {
     kill_background_apps
     wm size $(($(wm size | awk '{print $3}' | cut -dx -f1) / 3 * 2))x$(($(wm size | awk '{print $3}' | cut -dx -f2) / 3 * 2))
     wm density $(($(wm density | sed -n '1p' | cut -c19-) / 3 * 2))
-
 }
 
 function disable {
@@ -35,7 +56,7 @@ function disable {
 }
 
 case $1 in
-    "eficient") eficient ;;
+    "enable") eficient ;;
     "disable") disable ;;
     "*") echo "Unknown Command" ;;
 esac
